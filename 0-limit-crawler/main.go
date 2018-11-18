eea54b6a074c7bb77009defa4d831534537cedf0
Chris Armstrong <chris@chrisarmstrong.me>
2018-11-17 17:10:00 -0800

    Attempt problem 1

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,6 +12,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
@@ -32,9 +33,14 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	fmt.Printf("found: %s %q\n", url, body)
 
 	wg.Add(len(urls))
+
+	rate := time.Second
+	throttle := time.Tick(rate)
+
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
+		<-throttle
 		go Crawl(u, depth-1, wg)
 	}
 	return
